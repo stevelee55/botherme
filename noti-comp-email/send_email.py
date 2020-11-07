ef06ae53992ecaac3e9906fdfc3a9db764ba007c
yjheo15 <yjheo@umich.edu>
2020-11-07 17:50:19 -0500

    static email sender

--- noti-comp-email/send_email.py
+++ noti-comp-email/send_email.py
@@ -2,3 +2,26 @@
 # from smtplib import SMTP
 import smtplib, ssl
 
+
+port = 465  # for SSL
+
+sender_email = "botherme.notifier@gmail.com"
+password = "letsrockthis1!"
+
+receiver_email = "yjheo@umich.edu"
+# receiver_email = input("Receiver_email: ")
+
+# message = input("message content (first line has to start with 'Subject: ' then have an empty line: ")
+
+message = """\
+Subject: Botherme Notification
+
+Hey do ur stuff bruh"""
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server:
+    server.login("botherme.notifier@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message)
